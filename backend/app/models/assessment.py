1132cad0d0bc959b669e151e6ff0aa532ec5b77e
BhyllionSolomon <olagunjukoredesolomon@gmail.com>
2026-08-12 21:24:06 +0100

    Update assessment.py

--- backend/app/models/assessment.py
+++ backend/app/models/assessment.py
@@ -9,8 +9,8 @@ from app.core.database import get_db
 from app.models.student import Student
 
 from app.models.user import User
-from app.api.v1.auth import get_current_user
-from app.models.assessment import Assessment
+
+
 
 router = APIRouter(
     prefix="/imports",
